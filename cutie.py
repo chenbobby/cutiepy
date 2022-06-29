96395c51595a3585b40d94f972fee3cfec79ea21
Bobby Chen <chenbobby168@gmail.com>
2022-06-29 16:31:38 +0900

    Creates fast and slow job for cutie script.

--- cutie.py
+++ cutie.py
@@ -6,10 +6,17 @@ registry = cutiepy.Registry(broker_url="http://localhost:4000")
 
 
 @registry.job
-def wait():
+def fast():
     time.sleep(0.1)
 
 
+@registry.job
+def slow():
+    time.sleep(1)
+
+
 if __name__ == "__main__":
-    job_id = wait.enqueue()
-    print(f"Enqueued a job with ID {job_id}")
+    job_id = fast.enqueue()
+    print(f"Enqueued a `fast` job with ID {job_id}")
+    job_id = slow.enqueue()
+    print(f"Enqueued a `slow` job with ID {job_id}")
